0d51bff1937d24f59de7c53c0d5c584b0ecad2dd
Clifford Crerar <Clifford.Crerar@AdaptIT.co.za>
2017-12-08 18:21:44 +0200

    Commit this

--- js/home.js
+++ js/home.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    var carImgHeight = $('#group1').height();
-    var carImgWidth = $('#group1').width();
-    console.log(carImgHeight);
-    console.log(carImgWidth);
-    var imgElement = $('img');
-    $.each(imgElement, function(i) {
-        imgElement[i].height = carImgHeight;
-        imgElement[i].width = carImgWidth;
-    });
+	var carImgHeight = $('#group1').height();
+	var carImgWidth = $('#group1').width();
+	console.log(carImgHeight);
+	console.log(carImgWidth);
+	var imgElement = $('img');
+	$.each(imgElement, function(i) {
+		imgElement[i].height = carImgHeight;
+		imgElement[i].width = carImgWidth;
+	});
 });
