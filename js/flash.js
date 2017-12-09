9c8d1c1efa3a75f3de326446aef40bccee46adf2
CliffordCrerar <cliff.crerar@gmail.com>
2017-12-09 14:45:34 +0200

    This stuff

---
+++ js/flash.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    //Fade in and out scroll instruction
+    setTimeout(function() {
+        $('div.row.alert').fadeIn('slow').delay(2000).fadeOut('slow');
+    }, 2000);
+
+    //Start flashin scroll down icon
+    setTimeout(function() {
+        $('#scrlprompt').addClass('flash');
+    }, 3000);
+
+});
--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
     //control the navbar click
-
-
-
     $('nav.navbar').on('click', function(Event) {
         $('a.nav-link.active').removeClass('active');
         if (Event.target.className == 'nav-link') {
@@ -10,27 +7,39 @@ $(document).ready(function() {
         }
     });
 
-
-    /*
+    //Change the navbar according to section in view
     $('.parallax__group').mouseenter(function(Event) {
-        console.log('enter new group')
-            //console.log(Event);
-            //console.log(Event.currentTarget);
-        var scrGroup = Event.currentTarget.id;
-
+        $('a.nav-link.active').removeClass('active');
+        var srcGroup = Event.currentTarget.id;
         $('.nav-link').each(function(i) {
-
             var navGroup = $('a.nav-link').get(i).attributes[1].value.replace('#', '');
-            //var selector = 'nav.navbar:eq(' + i + 1 + ')';
-            //console.log($(selector));
-
-            if (scrGroup == navGroup) {
-                console.log('NAV = ' + navGroup + ' SCR: ' + scrGroup);
-                //console.log($(selector));
+            if (srcGroup == navGroup) {
+                console.log('NAV = ' + navGroup + ' SCR: ' + srcGroup);
+                $('a.nav-link').get(i).classList.add('active');
+                switch (srcGroup) {
+                    case 'group1':
+                        $('a.navbar-brand').html('Welcome');
+                        break;
+                    case 'group2':
+                        $('a.navbar-brand').html('Title');
+                        break;
+                    case 'group3':
+                        $('a.navbar-brand').html('About');
+                        break;
+                    case 'group4':
+                        $('a.navbar-brand').html('Portfolio');
+                        break;
+                    case 'group5':
+                        $('a.navbar-brand').html('Socila');
+                        break;
+                    case 'group6':
+                        $('a.navbar-brand').html('Email');
+                        break;
+                    case 'group7':
+                        $('a.navbar-brand').html('Thanks!');
+                        break;
+                }
             }
         });
-    });*/
-
-
-
+    });
 });
