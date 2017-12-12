04f661924c68d7d68f0ec9c9863ec4e40b1b52a7
Cliff <cliff.crerar@gmail.com>
2017-12-12 23:09:03 +0200

    COmmit this

--- js/endlessRoll.js
+++ js/endlessRoll.js
@@ -7,32 +7,32 @@ $(document).ready(function() {
         $('#carousel2').mouseleave(function() {
             $('div.c2.ss-item').css({ 'animation-play-state': 'Running' });
         });
-    }, 11000);
+    }, 10);
     /*
-    $('div.c2.ss-item').css({ 'animation-play-state': 'Paused' });
-    $('div.card').hide();
+                $('div.c2.ss-item').css({ 'animation-play-state': 'Paused' });
+                $('div.card').hide();
     
-    var run1time = 0;
+                var run1time = 0;
 
-    $('#group4').mouseenter(function() {
-        if (run1time == 0) {
-            //$('div.card').fadeIn(5000)
-            setTimeout(function() {
-                var timeout = 0;
-                $('div.card').each(function(i) {
-                    timeout = timeout + 1000;
-                    var cardDiv = $(this);
-                    setTimeout(function() {
-                        cardDiv.fadeIn(3000);
-                        if (i == 4) {
-                            $('div.c2.ss-item').css({ 'animation-play-state': 'Running' });
-                        }
-                    }, timeout);
-                });
-            }, 2000);
-        }
-        run1time = 1;
-    });*/
+                $('#group4').mouseenter(function() {
+                    if (run1time == 0) {
+                        //$('div.card').fadeIn(5000)
+                        setTimeout(function() {
+                            var timeout = 0;
+                            $('div.card').each(function(i) {
+                                timeout = timeout + 1000;
+                                var cardDiv = $(this);
+                                setTimeout(function() {
+                                    cardDiv.fadeIn(3000);
+                                    if (i == 4) {
+                                        $('div.c2.ss-item').css({ 'animation-play-state': 'Running' });
+                                    }
+                                }, timeout);
+                            });
+                        }, 2000);
+                    }
+                    run1time = 1;
+                });*/
 });
 /*
 animation: slideshow 10s linear infinite;
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,50 +1,70 @@
 $(document).ready(function() {
-    $('div.card-bottom>input.btn').tooltip({ delay: { "show": 1000, "hide": 500 } });
+    $('div.card-bottom>input.btn').tooltip({ delay: { show: 1000, hide: 500 } });
 
-    $('div.card-bottom>input').on('click', function(event) {
-
-    })
+    $('div.card-bottom>input').on('click', function(event) {});
 
     var CardData = {
         card1: {
             img: 'image/tributePage.png',
             link: 'https://codepen.io/Cliffenator/full/qmLRPr/',
-            text: 'First Page Ever, Single Static page. Composed from HTML and and element level style. No libraries used and not mobile responsive'
+            text: 'First Page Ever, Single Static page. Composed from HTML and and element level style. No libraries used and not mobile responsive',
         },
         card2: {
             img: 'image/qouteMachine.png',
             link: 'https://codepen.io/Cliffenator/full/wedePx/',
-            text: ''
+            text: 'Site that generates random quotes, mobile responsive, quotes object was scripted. Composed with HTML, CSS and "Vanila" javascript.',
         },
         card3: {
             img: 'image/wikiVeiwer.png',
             link: 'https://codepen.io/Cliffenator/full/GMNOXG/',
-            text: ''
+            text: 'Scripted using the WikiPedia API, Site can search WikiPedia as well as return results. Composed with HTML, CSS and jQuery',
         },
         card4: {
             img: 'image/tqoffroad.png',
             link: 'http://tqoffroadparts.com/#',
-            text: ''
+            text: 'My first "real" website to be used for business, customer is a retailer of after market offroad parts. Scripted with HTML, CSS and jQuery mixed with vanilla. Data is stored in JS objects.',
         },
         card5: {
-            img: '',
-            link: '',
-            text: ''
+            img: '/image/oldPort.png',
+            link: 'https://codepen.io/Cliffenator/full/rwBOVM/',
+            text: 'This is my old portfolio, my second freeCodeCamp project, HTML, CSS and jQuery',
         },
         card6: {
-            img: '',
-            link: '',
-            text: ''
+            img: 'image/future2.png',
+            link: 'f',
+            text: 'Future Real Estate',
         },
         card7: {
-            img: '',
-            link: '',
-            text: ''
+            img: 'image/future2.png',
+            link: 'f',
+            text: 'Future Real Estate',
         },
         card8: {
-            img: '',
-            link: '',
-            text: ''
+            img: 'image/future2.png',
+            link: 'f',
+            text: 'Future Real Estate',
         },
     };
+
+    //console.log(CardData);
+    $('div.pr1.col-md-3').each(function(i) {
+        var numCh = [11, 10, 9, 8];
+
+        var card;
+        var num = i;
+        if (numCh.indexOf(num) > -1) {
+            num = num - 8;
+
+            card = CardData['card' + (num + 1)];
+        } else {
+            card = CardData['card' + (i + 1)];
+        }
+
+        if (card.link !== 'F') {
+            $('div#card' + (i + 1) + '>img.card-img-top').attr('src', card.img);
+            $('div#card' + (i + 1) + '>div.card-body>p.card-text').html(card.text);
+            $('div#card' + (i + 1) + '>div.card-bottom>a').attr('href', card.link);
+            $('div#card' + (i + 1) + '>div.card-bottom>a').attr('target', '_blank');
+        }
+    });
 });
