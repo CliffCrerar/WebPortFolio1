03ce4a91ac79e270311807023d59a3dac340a8e4
CliffordCrerar <cliff.crerar@gmail.com>
2017-12-16 22:04:03 +0200

    asd

--- js/collapseToggle.js
+++ js/collapseToggle.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
-    $('div.card.tel').click(function(event) {
-        console.log($(this))
+
+    $('div.card.tel').click(function() {
+        console.log($(this));
 
         $('#collapseOne').toggle('collapse');
 
         var isCollapsed = $('div.left-angle>button').hasClass('fa-angle-left');
-        console.log(isCollapsed)
+        console.log(isCollapsed);
 
         if (isCollapsed) {
-            console.log(isCollapsed)
-            $('div.left-angle>button').removeClass('fa-angle-left');
-            $('div.left-angle>button').addClass('fa-angle-down');
+            console.log(isCollapsed);
+            //$('div.left-angle>button').removeClass('fa-angle-left');
+            //$('div.left-angle>button').addClass('fa-angle-down');
+            $('div.left-angle>button').css("transform", "rotateOutDownRight");
         } else {
-            console.log(isCollapsed)
-            $('div.left-angle>button').removeClass('fa-angle-left');
-            $('div.left-angle>button').addClass('fa-angle-left');
+            console.log(isCollapsed);
+            //$('div.left-angle>button').removeClass('fa-angle-down');
+            //$('div.left-angle>button').addClass('fa-angle-left');
         }
     });
 });
