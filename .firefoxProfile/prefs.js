dd20c3926db66fa581874e54039e7bb14d13b62b
Cliff <cliff.crerar@gmail.com>
2017-12-21 22:24:52 +0200

    reverting

--- .firefoxProfile/prefs.js
+++
@@ -1,5 +0,0 @@
-
-user_pref("browser.shell.checkDefaultBrowser", false);
-user_pref("devtools.chrome.enabled", true);
-user_pref("devtools.debugger.prompt-connection", false);
-user_pref("devtools.debugger.remote-enabled", true);
