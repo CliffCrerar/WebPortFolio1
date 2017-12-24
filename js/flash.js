8a8ba9e85c17dfc942106b2bff1b1a5e013789eb
Cliff <cliff.crerar@gmail.com>
2017-12-24 23:52:45 +0200

    Commit this

--- js/flash.js
+++ js/flash.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     //Fade in and out scroll instruction
     setTimeout(function() {
-        $('div.row.alert').fadeIn('slow').delay(2000).fadeOut('slow');
-    }, 2000);
+        $('.scrMsg').fadeIn('slow').delay(2000).fadeOut('slow');
+    }, 4000);
 
     //Start flashin scroll down icon
     setTimeout(function() {
         $('#scrlprompt').addClass('flash');
-    }, 3000);
+    }, 5000);
 });
