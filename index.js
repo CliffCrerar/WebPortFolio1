c0b5eba7519e4975977356ce255b6613a2c40630
Clifford Crerar <cliff.crerar@gmail.com>
2018-04-30 03:24:06 +0200

    install loaders

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * long description for the file
+ *
+ * @summary Site entry point
+ * @author Cliff Crerar
+ *
+ * Created at     : 2018-04-30 03:14:56 
+ * Last modified  : 2018-04-30 03:14:56 
+ */
+
---
+++ webpack.config.dev.js
@@ -0,0 +1,72 @@
+/**
+ * long description for the file
+ *
+ * @summary Webpack configuration file
+ * @author Cliff Crerar
+ *
+ * Created at     : 2018-04-30 03:04:54 
+ * Last modified  : 2018-04-30 03:23:43
+ */
+
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: ['url-loader?limit=10000&mimetype=application/font-woff']
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader' // creates style nodes from JS strings
+                    },
+                    {
+                        loader: 'css-loader', // translates CSS into CommonJS
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            { test: /\.(png|svg|jpg|gif|jpeg)$/, use: ['file-loader'] },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,
+                            removeComments: true,
+                            collapseWhitespace: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Cliff Crerar Portfolio',
+            favicon: './favicon.ico'
+        })
+
+    ]
+
+}
